8b709d2d04f819cbd164f8b0f1d4b9d6c367fbef
Eric Tuvesson <eric.tuvesson@gmail.com>
2021-04-15 17:35:11 +0200

    feat: add unit tests

--- test/Mallos.Searchable.Test/SearchableTest.cs
+++ test/Mallos.Searchable.Test/SearchableTest.cs
@@ -43,30 +43,166 @@ namespace Mallos.Searchable.Test
             IsFilters.Add(new IsOne());
             Filters.Add(new Value());
         }
+
+        protected override IEnumerable<TestObject> FreeTextFilter(IEnumerable<TestObject> values, bool negative, string text)
+        {
+            return base.FreeTextFilter(values, negative, text);
+        }
     }
 
     public class SearchableTest
     {
-        [Fact]
-        public void Test1()
+        readonly TestObjectSearchable searchable = new TestObjectSearchable();
+        readonly TestObject[] testdata = new TestObject[]
         {
-            var searchable = new TestObjectSearchable();
+            new ("1"),
+            new ("2"),
+            new ("3")
+        };
 
+        [Fact]
+        public void Analyze_IsFilter_ValueMatch()
+        {
+            // Arrange
+            var query = "is:one";
 
-            var testdata = new TestObject[]
-            {
-                new ("1"),
-                new ("2"),
-                new ("3")
-            };
+            // Act
+            var analyzed = searchable.Analyze(query);
 
+            // Assert
+            Assert.Single(analyzed);
+            Assert.False(analyzed[0].IsNegative);
+            Assert.Equal("is", analyzed[0].Key);
+            Assert.Equal("one", analyzed[0].Value);
+        }
 
-            var analyzed = searchable.Analyze("is:one -is:one eric -eric value:1 -value:2");
-            var result1 = searchable.Search(testdata, "is:one").ToList();
-            var result2 = searchable.Search(testdata, "-is:one").ToList();
-            var result3 = searchable.Search(testdata, "value:3").ToList();
+        [Fact]
+        public void Analyze_IsFilter_NegativeValueMatch()
+        {
+            // Arrange
+            var query = "-is:one";
 
-            // TODO: Write unit tests
+            // Act
+            var analyzed = searchable.Analyze(query);
+
+            // Assert
+            Assert.Single(analyzed);
+            Assert.True(analyzed[0].IsNegative);
+            Assert.Equal("is", analyzed[0].Key);
+            Assert.Equal("one", analyzed[0].Value);
+        }
+
+        [Fact]
+        public void Analyze_IsFilter_TableizedValueMatch()
+        {
+            // Arrange
+            var query = "is:\"o-n-e\"";
+            // NOTE: This have to be a string otherwise it wont recognize it.
+
+            // Act
+            var analyzed = searchable.Analyze(query);
+
+            // Assert
+            Assert.Single(analyzed);
+            Assert.False(analyzed[0].IsNegative);
+            Assert.Equal("is", analyzed[0].Key);
+            Assert.Equal("o-n-e", analyzed[0].Value);
+        }
+
+        [Fact]
+        public void Analyze_IsFilter_StringValueMatch()
+        {
+            // Arrange
+            var query = "is:\"one\"";
+
+            // Act
+            var analyzed = searchable.Analyze(query);
+
+            // Assert
+            Assert.Single(analyzed);
+            Assert.False(analyzed[0].IsNegative);
+            Assert.Equal("is", analyzed[0].Key);
+            Assert.Equal("one", analyzed[0].Value);
+        }
+
+        [Fact]
+        public void Analyze_Filter_ValueMatch()
+        {
+            // Arrange
+            var query = "key:value";
+
+            // Act
+            var analyzed = searchable.Analyze(query);
+
+            // Assert
+            Assert.Single(analyzed);
+            Assert.False(analyzed[0].IsNegative);
+            Assert.Equal("key", analyzed[0].Key);
+            Assert.Equal("value", analyzed[0].Value);
+        }
+
+        [Fact]
+        public void Analyze_Filter_NegativeValueMatch()
+        {
+            // Arrange
+            var query = "-key:value";
+
+            // Act
+            var analyzed = searchable.Analyze(query);
+
+            // Assert
+            Assert.Single(analyzed);
+            Assert.True(analyzed[0].IsNegative);
+            Assert.Equal("key", analyzed[0].Key);
+            Assert.Equal("value", analyzed[0].Value);
+        }
+
+        [Fact]
+        public void Analyze_FreeText_ValueChainMatch()
+        {
+            // Arrange
+            var query = "my free text";
+
+            // Act
+            var analyzed = searchable.Analyze(query);
+
+            // Assert
+            Assert.Equal(3, analyzed.Count);
+            Assert.False(analyzed[0].IsNegative);
+            Assert.False(analyzed[1].IsNegative);
+            Assert.False(analyzed[2].IsNegative);
+            Assert.Equal("my", analyzed[0].Value);
+            Assert.Equal("free", analyzed[1].Value);
+            Assert.Equal("text", analyzed[2].Value);
+        }
+
+        [Fact]
+        public void Analyze_FreeText_StringValueMatch()
+        {
+            // Arrange
+            var query = "\"my free text\"";
+
+            // Act
+            var analyzed = searchable.Analyze(query);
+
+            // Assert
+            Assert.Single(analyzed);
+            Assert.Equal("my free text", analyzed[0].Value);
+        }
+
+        [Fact]
+        public void Analyze_FreeText_NegativeValueMatch()
+        {
+            // Arrange
+            var query = "-negative";
+
+            // Act
+            var analyzed = searchable.Analyze(query);
+
+            // Assert
+            Assert.Single(analyzed);
+            Assert.True(analyzed[0].IsNegative);
+            Assert.Equal("negative", analyzed[0].Value);
         }
     }
 }
